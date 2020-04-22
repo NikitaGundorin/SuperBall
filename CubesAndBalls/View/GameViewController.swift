c4f209f90b38c4eba095392f3fca497e13170d3b
NikitaGundorin <djnig@yandex.ru>
2020-04-22 20:17:20 +0300

    Code cleanup

---
+++ CubesAndBalls/View/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  CubesAndBalls
+//
+//  Created by Никита Гундорин on 21.04.2020.
+//  Copyright © 2020 Nikita Gundorin. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class GameViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var ballButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    let viewModel = GameViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ballButton.layer.cornerRadius = ballButton.layer.frame.width / 2
+        ballButton.isEnabled = false
+        
+        sceneView.delegate = self
+        sceneView.scene.physicsWorld.contactDelegate = viewModel
+        viewModel.vc = self
+        
+        let light = SCNLight()
+        light.type = .omni
+        let lightNode = SCNNode()
+        lightNode.light = light
+        sceneView.pointOfView?.addChildNode(lightNode)
+        viewModel.addTargetNodes()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    @IBAction func ballTouched(_ sender: Any) {
+        viewModel.throwBall()
+    }
+}
--- CubesAndBalls/ViewModels/GameViewModel.swift
+++ CubesAndBalls/ViewModels/GameViewModel.swift
@@ -1,74 +1,34 @@
 //
-//  ViewController.swift
+//  PhysicsContactDelegate.swift
 //  CubesAndBalls
 //
-//  Created by Никита Гундорин on 21.04.2020.
+//  Created by Никита Гундорин on 22.04.2020.
 //  Copyright © 2020 Nikita Gundorin. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-    
-    @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var ballButton: UIButton!
-    @IBOutlet weak var scoreLabel: UILabel!
+class GameViewModel: NSObject, SCNPhysicsContactDelegate {
+    weak var vc: GameViewController?
     
     var score = 0 {
         didSet {
-            scoreLabel.text = "Score: \(score)"
+            vc?.scoreLabel.text = "Score: \(score)"
         }
     }
     
     var ballColor: Color! {
         didSet {
-            ballButton.backgroundColor = ballColor.value
+            vc?.ballButton.backgroundColor = ballColor.value
         }
     }
     
     var colors: [Color] = []
     
-    let physicsContactDelegate = PhysicsContactDelegate()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        ballButton.layer.cornerRadius = ballButton.layer.frame.width / 2
-        ballButton.isEnabled = false
-        
-        sceneView.delegate = self
-        sceneView.scene.physicsWorld.contactDelegate = physicsContactDelegate
-        physicsContactDelegate.viewController = self
-        
-        let light = SCNLight()
-        light.type = .omni
-        let lightNode = SCNNode()
-        lightNode.light = light
-        sceneView.pointOfView?.addChildNode(lightNode)
-        addTargetNodes()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let configuration = ARWorldTrackingConfiguration()
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        sceneView.session.pause()
-    }
-    
-    @IBAction func ballTouched(_ sender: Any) {
-        throwBall(withColor: ballColor)
-    }
-    
     func setBallButton() {
-        if ballButton.isEnabled || colors.count == 0 {
+        if vc != nil && vc!.ballButton.isEnabled || colors.count == 0 {
             return
         }
         var color: Color
@@ -76,14 +36,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             color = Color.random()
         } while !self.colors.contains(color)
         ballColor = color
-        ballButton.isEnabled = true
+        vc?.ballButton.isEnabled = true
     }
     
     func endGame(message: String) {
         print(message)
     }
     
-    private func addTargetNodes() {
+    func addTargetNodes() {
         for _ in 1...10 {
             let size = CGFloat(0.5)
             let color = Color.random()
@@ -105,14 +65,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             node.physicsBody?.categoryBitMask = CollisionCategory.boxCategory.rawValue
             node.physicsBody?.contactTestBitMask = CollisionCategory.ballCategory.rawValue
             
-            sceneView.scene.rootNode.addChildNode(node)
+            vc?.sceneView.scene.rootNode.addChildNode(node)
         }
         setBallButton()
     }
     
-    private func throwBall(withColor color: Color) {
+    func throwBall() {
         let ball = SCNSphere(radius: 0.5)
-        ball.firstMaterial?.diffuse.contents = color.value
+        ball.firstMaterial?.diffuse.contents = ballColor.value
         let node = SCNNode(geometry: ball)
         node.name = "ball"
         
@@ -128,8 +88,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.physicsBody?.applyForce(nodeDirection, at: SCNVector3(0.1,0,0), asImpulse: true)
         node.physicsBody?.applyForce(nodeDirection, asImpulse: true)
         
-        sceneView.scene.rootNode.addChildNode(node)
-        ballButton.isEnabled = false
+        vc?.sceneView.scene.rootNode.addChildNode(node)
+        vc?.ballButton.isEnabled = false
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             node.removeFromParentNode()
@@ -146,7 +106,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     private func getUserVector() -> (SCNVector3, SCNVector3) {
-        if let frame = self.sceneView.session.currentFrame {
+        if let frame = vc?.sceneView.session.currentFrame {
             let mat = SCNMatrix4(frame.camera.transform)
             let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
             let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
@@ -154,5 +114,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return (dir, pos)
         }
         return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
+    }
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        guard let nodeAPhysicsBody = contact.nodeA.physicsBody,
+            let nodeBPhysicsBody = contact.nodeB.physicsBody,
+            nodeAPhysicsBody.categoryBitMask == CollisionCategory.boxCategory.rawValue
+                && nodeBPhysicsBody.categoryBitMask == CollisionCategory.ballCategory.rawValue
+                || nodeAPhysicsBody.categoryBitMask == CollisionCategory.ballCategory.rawValue
+                && nodeBPhysicsBody.categoryBitMask == CollisionCategory.boxCategory.rawValue
+            else { return }
+        
+        guard let nodeAColor = contact.nodeA.geometry?.firstMaterial?.diffuse.contents as? UIColor,
+            let nodeBColor = contact.nodeB.geometry?.firstMaterial?.diffuse.contents as? UIColor,
+            nodeAColor == nodeBColor
+            else {
+                DispatchQueue.main.async {
+                    self.endGame(message: "wrong color")
+                }
+                return
+        }
+        
+        DispatchQueue.main.async {
+            contact.nodeA.removeFromParentNode()
+            contact.nodeB.removeFromParentNode()
+            self.score += 1
+            if let color = Color.value(ofUIColor: nodeAColor),
+                let index = self.colors.lastIndex(of: color) {
+                self.colors.remove(at: index)
+                self.setBallButton()
+            }
+        }
+        
+        let explosion = SCNParticleSystem(named: "Explode", inDirectory: nil)
+        explosion?.particleColor = nodeAColor
+        contact.nodeB.addParticleSystem(explosion!)
     }
 }
