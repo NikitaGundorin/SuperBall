df2a53a5d0af7f03ae42889fab444a1eaeca2efd
NikitaGundorin <djnig@yandex.ru>
2020-04-22 20:36:38 +0300

    Add start screen

---
+++ CubesAndBalls/View/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  CubesAndBalls
+//
+//  Created by Никита Гундорин on 22.04.2020.
+//  Copyright © 2020 Nikita Gundorin. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
